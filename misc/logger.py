eba7583099c179bff8a31fba0217c1424d1ba7b8
louis JULIEN <louis69600@laposte.net>
2025-02-18 18:02:43 +0100

    remove werkzeug

--- misc/logger.py
+++ misc/logger.py
@@ -7,6 +7,3 @@ logger.setLevel(logging.DEBUG)
 # Create a console handler for Pycharm and set the formatter
 console_handler = logging.StreamHandler()
 logger.addHandler(console_handler)
-
-werkzeug_log = logging.getLogger('werkzeug')
-werkzeug_log.setLevel(logging.ERROR)
